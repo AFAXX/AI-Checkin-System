0573bf398cdb29310c3437d6cf059027bd234d27
AFAXX <hiddenmonk.yt@gmail.com>
2026-07-24 08:26:58 +0200

    Add files via upload

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -8,11 +8,11 @@ export function useIsMobile() {
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      setIsMobile(mql.matches)
     }
     mql.addEventListener("change", onChange)
-    // Initial value via onChange callback (not direct setState in effect body)
-    onChange()
+    // Use queueMicrotask to avoid synchronous setState in effect body
+    queueMicrotask(() => setIsMobile(mql.matches))
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
